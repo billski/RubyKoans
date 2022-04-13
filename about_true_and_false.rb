4d80f936107bf210080879871d0a0a8b232bca25
Billski <Bill@bill-tucker.com>
2022-04-13 10:02:57 -0700

    True and False

--- about_true_and_false.rb
+++ about_true_and_false.rb
@@ -3,31 +3,31 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 class AboutTrueAndFalse < Neo::Koan
   def truth_value(condition)
     if condition
-      :true_stuff
+      "FILL ME IN"
     else
-      :false_stuff
+      "FILL ME IN"
     end
   end
 
   def test_true_is_treated_as_true
-    assert_equal __, truth_value(true)
+    assert_equal "FILL ME IN", truth_value(true)
   end
 
   def test_false_is_treated_as_false
-    assert_equal __, truth_value(false)
+    assert_equal "FILL ME IN", truth_value(false)
   end
 
   def test_nil_is_treated_as_false_too
-    assert_equal __, truth_value(nil)
+    assert_equal "FILL ME IN", truth_value(nil)
   end
 
   def test_everything_else_is_treated_as_true
-    assert_equal __, truth_value(1)
-    assert_equal __, truth_value(0)
-    assert_equal __, truth_value([])
-    assert_equal __, truth_value({})
-    assert_equal __, truth_value("Strings")
-    assert_equal __, truth_value("")
+    assert_equal "FILL ME IN", truth_value(1)
+    assert_equal "FILL ME IN", truth_value(0)
+    assert_equal "FILL ME IN", truth_value([])
+    assert_equal "FILL ME IN", truth_value({})
+    assert_equal "FILL ME IN", truth_value("Strings")
+    assert_equal "FILL ME IN", truth_value("")
   end
 
 end
